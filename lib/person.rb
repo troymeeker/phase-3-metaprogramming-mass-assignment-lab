4bc23801c2d1514d2aa0c4a1ad7e7578927e4072
troymeeker <troymeeker1@gmail.com>
2021-12-22 15:25:41 -0800

    commit tree 945a25f1f0f5ea4dbef5d68d87eca884ea6fb569

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
   # your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    # @name = name
+    # @birthday = birthday
+    # @hair_color = hair_color
+    # @eye_color = eye_color
+    # @height = height
+    # @weight = weight
+    # @handed = handed
+    # @complexion = complexion
+    # @t_shirt_size = t_shirt_size
+    # @wrist_size = wrist_size
+    # @glove_size = glove_size
+    # @pant_length = pant_length
+    # @pant_width = pant_width
+
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
